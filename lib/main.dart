bebe5000f378f917bbfd5b968347d77b2d1d384a
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-02-22 17:55:39 +1100

    Added changelogs viewer

--- lib/main.dart
+++ lib/main.dart
@@ -214,6 +214,57 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> tryGithubChangelogs() async {
+    late GithubReleaseLatest releaseLatest;
+    final response = await http.get(
+      Uri.parse(
+          'https://api.github.com/repos/jumpyjacko/resentral_flutter/releases/latest'),
+    );
+    if (response.statusCode == 200) {
+      releaseLatest = GithubReleaseLatest.fromJson(jsonDecode(response.body));
+      await popupGithubChangelogs(releaseLatest);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Failed to get changelogs, try again',
+            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+      );
+      throw Exception('Failed to fetch: ${response.statusCode}');
+    }
+  }
+
+  Future<void> popupGithubChangelogs(GithubReleaseLatest response) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            title: const Text('Latest Changelogs'),
+            content: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SingleChildScrollView(
+                child: Text(
+                  response.body.toString(),
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -302,6 +353,10 @@ class _HomePageState extends State<HomePage> {
                   value: 2,
                   child: const Text("Check for Updates"),
                 ),
+                const PopupMenuItem(
+                  value: 3,
+                  child: const Text("Changelogs"),
+                ),
               ],
               offset: Offset(0, AppBar().preferredSize.height),
               onSelected: (value) {
@@ -312,6 +367,9 @@ class _HomePageState extends State<HomePage> {
                     break;
                   case 2:
                     tryOtaUpdate();
+                    break;
+                  case 3:
+                    tryGithubChangelogs();
                     break;
                   default:
                 }
@@ -372,6 +430,255 @@ class Commit {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['sha'] = this.sha;
     data['url'] = this.url;
+    return data;
+  }
+}
+
+// Why ;-; (also auto-generated)
+class GithubReleaseLatest {
+  String? url;
+  String? assetsUrl;
+  String? uploadUrl;
+  String? htmlUrl;
+  int? id;
+  Author? author;
+  String? nodeId;
+  String? tagName;
+  String? targetCommitish;
+  String? name;
+  bool? draft;
+  bool? prerelease;
+  String? createdAt;
+  String? publishedAt;
+  List<Assets>? assets;
+  String? tarballUrl;
+  String? zipballUrl;
+  String? body;
+
+  GithubReleaseLatest(
+      {this.url,
+      this.assetsUrl,
+      this.uploadUrl,
+      this.htmlUrl,
+      this.id,
+      this.author,
+      this.nodeId,
+      this.tagName,
+      this.targetCommitish,
+      this.name,
+      this.draft,
+      this.prerelease,
+      this.createdAt,
+      this.publishedAt,
+      this.assets,
+      this.tarballUrl,
+      this.zipballUrl,
+      this.body});
+
+  GithubReleaseLatest.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    assetsUrl = json['assets_url'];
+    uploadUrl = json['upload_url'];
+    htmlUrl = json['html_url'];
+    id = json['id'];
+    author =
+        json['author'] != null ? new Author.fromJson(json['author']) : null;
+    nodeId = json['node_id'];
+    tagName = json['tag_name'];
+    targetCommitish = json['target_commitish'];
+    name = json['name'];
+    draft = json['draft'];
+    prerelease = json['prerelease'];
+    createdAt = json['created_at'];
+    publishedAt = json['published_at'];
+    if (json['assets'] != null) {
+      assets = <Assets>[];
+      json['assets'].forEach((v) {
+        assets!.add(new Assets.fromJson(v));
+      });
+    }
+    tarballUrl = json['tarball_url'];
+    zipballUrl = json['zipball_url'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['url'] = this.url;
+    data['assets_url'] = this.assetsUrl;
+    data['upload_url'] = this.uploadUrl;
+    data['html_url'] = this.htmlUrl;
+    data['id'] = this.id;
+    if (this.author != null) {
+      data['author'] = this.author!.toJson();
+    }
+    data['node_id'] = this.nodeId;
+    data['tag_name'] = this.tagName;
+    data['target_commitish'] = this.targetCommitish;
+    data['name'] = this.name;
+    data['draft'] = this.draft;
+    data['prerelease'] = this.prerelease;
+    data['created_at'] = this.createdAt;
+    data['published_at'] = this.publishedAt;
+    if (this.assets != null) {
+      data['assets'] = this.assets!.map((v) => v.toJson()).toList();
+    }
+    data['tarball_url'] = this.tarballUrl;
+    data['zipball_url'] = this.zipballUrl;
+    data['body'] = this.body;
+    return data;
+  }
+}
+
+class Author {
+  String? login;
+  int? id;
+  String? nodeId;
+  String? avatarUrl;
+  String? gravatarId;
+  String? url;
+  String? htmlUrl;
+  String? followersUrl;
+  String? followingUrl;
+  String? gistsUrl;
+  String? starredUrl;
+  String? subscriptionsUrl;
+  String? organizationsUrl;
+  String? reposUrl;
+  String? eventsUrl;
+  String? receivedEventsUrl;
+  String? type;
+  bool? siteAdmin;
+
+  Author(
+      {this.login,
+      this.id,
+      this.nodeId,
+      this.avatarUrl,
+      this.gravatarId,
+      this.url,
+      this.htmlUrl,
+      this.followersUrl,
+      this.followingUrl,
+      this.gistsUrl,
+      this.starredUrl,
+      this.subscriptionsUrl,
+      this.organizationsUrl,
+      this.reposUrl,
+      this.eventsUrl,
+      this.receivedEventsUrl,
+      this.type,
+      this.siteAdmin});
+
+  Author.fromJson(Map<String, dynamic> json) {
+    login = json['login'];
+    id = json['id'];
+    nodeId = json['node_id'];
+    avatarUrl = json['avatar_url'];
+    gravatarId = json['gravatar_id'];
+    url = json['url'];
+    htmlUrl = json['html_url'];
+    followersUrl = json['followers_url'];
+    followingUrl = json['following_url'];
+    gistsUrl = json['gists_url'];
+    starredUrl = json['starred_url'];
+    subscriptionsUrl = json['subscriptions_url'];
+    organizationsUrl = json['organizations_url'];
+    reposUrl = json['repos_url'];
+    eventsUrl = json['events_url'];
+    receivedEventsUrl = json['received_events_url'];
+    type = json['type'];
+    siteAdmin = json['site_admin'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['login'] = this.login;
+    data['id'] = this.id;
+    data['node_id'] = this.nodeId;
+    data['avatar_url'] = this.avatarUrl;
+    data['gravatar_id'] = this.gravatarId;
+    data['url'] = this.url;
+    data['html_url'] = this.htmlUrl;
+    data['followers_url'] = this.followersUrl;
+    data['following_url'] = this.followingUrl;
+    data['gists_url'] = this.gistsUrl;
+    data['starred_url'] = this.starredUrl;
+    data['subscriptions_url'] = this.subscriptionsUrl;
+    data['organizations_url'] = this.organizationsUrl;
+    data['repos_url'] = this.reposUrl;
+    data['events_url'] = this.eventsUrl;
+    data['received_events_url'] = this.receivedEventsUrl;
+    data['type'] = this.type;
+    data['site_admin'] = this.siteAdmin;
+    return data;
+  }
+}
+
+class Assets {
+  String? url;
+  int? id;
+  String? nodeId;
+  String? name;
+  Null? label;
+  Author? uploader;
+  String? contentType;
+  String? state;
+  int? size;
+  int? downloadCount;
+  String? createdAt;
+  String? updatedAt;
+  String? browserDownloadUrl;
+
+  Assets(
+      {this.url,
+      this.id,
+      this.nodeId,
+      this.name,
+      this.label,
+      this.uploader,
+      this.contentType,
+      this.state,
+      this.size,
+      this.downloadCount,
+      this.createdAt,
+      this.updatedAt,
+      this.browserDownloadUrl});
+
+  Assets.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    id = json['id'];
+    nodeId = json['node_id'];
+    name = json['name'];
+    label = json['label'];
+    uploader =
+        json['uploader'] != null ? new Author.fromJson(json['uploader']) : null;
+    contentType = json['content_type'];
+    state = json['state'];
+    size = json['size'];
+    downloadCount = json['download_count'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    browserDownloadUrl = json['browser_download_url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['url'] = this.url;
+    data['id'] = this.id;
+    data['node_id'] = this.nodeId;
+    data['name'] = this.name;
+    data['label'] = this.label;
+    if (this.uploader != null) {
+      data['uploader'] = this.uploader!.toJson();
+    }
+    data['content_type'] = this.contentType;
+    data['state'] = this.state;
+    data['size'] = this.size;
+    data['download_count'] = this.downloadCount;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    data['browser_download_url'] = this.browserDownloadUrl;
     return data;
   }
 }
