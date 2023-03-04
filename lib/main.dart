ad877c6d41f8b559cf50e12d80b90b554aba2bba
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-03-04 18:09:18 +1100

    Refactor update logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ota_update/ota_update.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:after_layout/after_layout.dart';
-import 'package:http/http.dart' as http;
-import 'package:package_info/package_info.dart';
-
-import 'dart:convert';
 
 import 'package:resentral/pages/daily_timetable.dart';
 import 'package:resentral/pages/announcements.dart';
@@ -15,6 +10,8 @@ import 'package:resentral/pages/full_timetable.dart';
 import 'package:resentral/pages/settings.dart';
 import 'package:resentral/pages/login.dart';
 import 'package:resentral/pages/about.dart';
+
+import 'package:resentral/pages/settings_subscreens/update.dart';
 
 void main() {
   runApp(const MyApp());
@@ -127,7 +124,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int index = 0;
-  late OtaEvent currentEvent;
 
   final screens = [
     DailyTimetablePage(key: UniqueKey()),
@@ -135,137 +131,10 @@ class _HomePageState extends State<HomePage> {
     FullTimetablePage(key: UniqueKey()),
   ];
 
-  Future<String> checkUpdateExists(bool startup) async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    late GithubTags remoteVersion;
-
-    final response = await http.get(
-      Uri.parse(
-          'https://api.github.com/repos/jumpyjacko/resentral_flutter/tags'),
-    );
-
-    if (response.statusCode == 200) {
-      remoteVersion = GithubTags.fromJson(jsonDecode(response.body)[0]);
-    } else {
-      throw Exception('Failed to fetch: ${response.statusCode}');
-    }
-
-    if (remoteVersion.name.toString() == packageInfo.version && !startup) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'No new update!',
-            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.background,
-        ),
-      );
-      return '';
-    } else if (remoteVersion.name.toString() == packageInfo.version &&
-        startup) {
-      return '';
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'There\'s a new update!',
-            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          action: SnackBarAction(
-            label: 'Update',
-            onPressed: () => tryOtaUpdate(),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.background,
-        ),
-      );
-      return remoteVersion.name.toString();
-    }
-  }
-
-  Future<void> tryOtaUpdate() async {
-    final update = await checkUpdateExists(false);
-    if (update.isEmpty) {
-      return;
-    }
-
-    try {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'Downloading update...',
-            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.background,
-        ),
-      );
-      OtaUpdate()
-          .execute(
-        'https://github.com/JumpyJacko/resentral_flutter/releases/download/$update/app-release.apk',
-        destinationFilename: 'app-release.apk',
-      )
-          .listen(
-        (OtaEvent event) {
-          setState(() => currentEvent = event);
-        },
-      );
-    } catch (e) {
-      throw Exception('Failed to update. Details: $e');
-    }
-  }
-
-  Future<void> tryGithubChangelogs() async {
-    late GithubReleaseLatest releaseLatest;
-    final response = await http.get(
-      Uri.parse(
-          'https://api.github.com/repos/jumpyjacko/resentral_flutter/releases/latest'),
-    );
-    if (response.statusCode == 200) {
-      releaseLatest = GithubReleaseLatest.fromJson(jsonDecode(response.body));
-      await popupGithubChangelogs(releaseLatest);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'Failed to get changelogs, try again',
-            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.background,
-        ),
-      );
-      throw Exception('Failed to fetch: ${response.statusCode}');
-    }
-  }
-
-  Future<void> popupGithubChangelogs(GithubReleaseLatest response) async {
-    return showDialog<void>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            scrollable: true,
-            title: Text(
-              'Latest Changelogs - \n${response.name}',
-              style: const TextStyle(fontSize: 20.0),
-            ),
-            content: Text(response.body.toString(),
-                style: const TextStyle(fontSize: 14.0)),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Close'),
-              ),
-            ],
-          );
-        });
-  }
-
   @override
   void initState() {
     super.initState();
-    checkUpdateExists(true);
+    checkUpdateExists(true, context);
   }
 
   @override
@@ -341,10 +210,6 @@ class _HomePageState extends State<HomePage> {
                 ),
                 const PopupMenuItem(
                   value: 2,
-                  child: Text("Check for Updates"),
-                ),
-                const PopupMenuItem(
-                  value: 3,
                   child: Text("Changelogs"),
                 ),
               ],
@@ -356,10 +221,7 @@ class _HomePageState extends State<HomePage> {
                         builder: (context) => const AboutPage()));
                     break;
                   case 2:
-                    tryOtaUpdate();
-                    break;
-                  case 3:
-                    tryGithubChangelogs();
+                    tryGithubChangelogs(context);
                     break;
                   default:
                 }
@@ -369,41 +231,5 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-  }
-}
-
-// Auto-generated, check out https://javiercbk.github.io/json_to_dart/
-class GithubTags {
-  String? name;
-
-  GithubTags({this.name});
-
-  GithubTags.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['name'] = name;
-    return data;
-  }
-}
-
-class GithubReleaseLatest {
-  String? name;
-  String? body;
-
-  GithubReleaseLatest({this.name, this.body});
-
-  GithubReleaseLatest.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    body = json['body'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['name'] = name;
-    data['body'] = body;
-    return data;
   }
 }
